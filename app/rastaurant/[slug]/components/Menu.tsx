0d2d66f95a9ffac38b850479ecaaaa7d65e85544
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-22 07:12:43 +0200

    menu

--- app/rastaurant/[slug]/components/Menu.tsx
+++ app/rastaurant/[slug]/components/Menu.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import MenuCard from "./MenuCard";
+import { Item } from "@prisma/client";
 
-export default function Menu() {
+export default function Menu({ menu }: { menu: Item[] }) {
   return (
     <main className="bg-white mt-5">
       <div>
@@ -9,7 +10,9 @@ export default function Menu() {
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
         <div className="flex flex-wrap justify-between">
-          <MenuCard />
+          {menu.map((item) => (
+            <MenuCard key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </main>
--- app/rastaurant/[slug]/menu/page.tsx
+++ app/rastaurant/[slug]/menu/page.tsx
@@ -1,12 +1,35 @@
+import { PrismaClient } from "@prisma/client";
 import Menu from "../components/Menu";
 import RastaurantHeader from "../components/RastaurantHeader";
 import RastaurantNavBar from "../components/RastaurantNavBar";
 
-export default function RastaurntMenu() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+  if (!restaurant) {
+    throw new Error();
+  }
+  return restaurant.items;
+};
+
+export default async function RastaurntMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const menu = await fetchRestaurantMenu(params.slug);
   return (
     <div className="bg-white w-[100%] rounded p-3 shadow">
-      <RastaurantNavBar />
-      <Menu />
+      <RastaurantNavBar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   );
 }
